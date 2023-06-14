8899cbe55d205ae8e35f0a491bbb6e5bcf7ed5c0
chenmoonmo <chenmoonmo@gmail.com>
2023-06-14 10:40:30 +0800

    fix: remove last version code

--- apps/pvp/src/components/Dialogs/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './ClosePostionDialog'
-export * from './CreateRoomDialog';
-export * from './NFTRoomDialog';
-export * from './OpenPositionDialog'
-export * from './OpenRoomDialog'
-export * from './WhitelistDialog'
--- apps/pvp/src/components/index.tsx
+++ apps/pvp/src/components/index.tsx
@@ -1,8 +1,6 @@
-export * from './Dialogs';
 export * from './Icons';
 export * from './LeverageRadio';
 export * from './NFTCard';
-export * from './OpponentInfo'
+export * from './OpponentInfo';
 export * from './TimeInput';
 export * from './TokenAmountInput';
-
