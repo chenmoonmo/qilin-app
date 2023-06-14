6c621641a16fb2e6b2f52eebf6a9c3fab5e9fc37
chenmoonmo <chenmoonmo@gmail.com>
2023-06-14 10:46:30 +0800

    fix: change animation_url

--- apps/pvp/src/constant/index.ts
+++ apps/pvp/src/constant/index.ts
@@ -1,4 +1,8 @@
-export * from './api'
-export * from './contracts'
-export * from './pairs'
+export * from './api';
+export * from './contracts';
+export * from './pairs';
 
+export const NFTBaseUrl =
+  process.env.NODE_ENV === 'production'
+    ? 'https://projectstriper.xyz/'
+    : 'http://localhost:3000';
