22ca0348af331a46e27c4c1436206a7bd8b61bc1
chenmoonmo <chenmoonmo@gmail.com>
2023-06-16 14:57:50 +0800

    fix: refetch dealertoplayer after create

--- apps/pvp/src/hooks/useDealerPoolInfo.ts
+++ apps/pvp/src/hooks/useDealerPoolInfo.ts
@@ -1,3 +1,4 @@
+import { useMemo } from 'react';
 import { useContractRead } from 'wagmi';
 
 import { CONTRACTS } from '@/constant';
@@ -6,12 +7,22 @@ import Dealer from '@/constant/abis/Dealer.json';
 import { usePoolInfo } from './usePoolInfo';
 
 export const useDealerPoolInfo = (dealerId: number) => {
-  const { data: playerId } = useContractRead({
+  const { data: playerId, refetch } = useContractRead({
     address: CONTRACTS.DealerAddress,
     abi: Dealer.abi,
     functionName: 'dealerToPlayer',
     args: [dealerId],
   });
 
-  return usePoolInfo(playerId as number);
+  const info = usePoolInfo(playerId as number);
+
+  return useMemo(() => {
+    return {
+      ...info,
+      refetch: () => {
+        info?.refetch();
+        refetch();
+      },
+    };
+  }, [info, refetch]);
 };
