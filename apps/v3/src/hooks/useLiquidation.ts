b0534944dc08b30762f6374706bd8eff964a5f16
chenmoonmo <chenmoonmo@gmail.com>
2023-07-05 14:49:00 +0800

    fix: leverage error

--- apps/v3/src/hooks/useLiquidation.ts
+++ apps/v3/src/hooks/useLiquidation.ts
@@ -12,7 +12,7 @@ export const useLiquidation = () => {
     chainId
       ? `/liquidation?chain_id=${chainId}&user_addres=${address}&asset_address=0x9705D3d013cB3dc406B8e29A0860E3686164199c&pool_address=0x0824Ca3F63f0E0f1Cf77808334b514D1278C6ef4`
       : null,
-    async url => {
+    async (url: string) => {
       const result = await fetcher<{
         liquidation_list: LiquidationItem[];
       }>(url, {
