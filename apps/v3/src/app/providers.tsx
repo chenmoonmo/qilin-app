78d19b381aea6c513a80f8e1d01954c2f3a0438d
chenmoonmo <chenmoonmo@gmail.com>
2023-08-30 17:58:59 +0800

    feat: use arbitrum and add claim btn

--- apps/v3/src/app/providers.tsx
+++ apps/v3/src/app/providers.tsx
@@ -1,12 +1,12 @@
 'use client';
 import { ToastProvider } from '@qilin/component';
 import Web3Provider from '@qilin/wagmi-provider';
-import { arbitrumGoerli } from 'wagmi/chains';
+import { arbitrum } from 'wagmi/chains';
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <Web3Provider
-      defaultChains={[arbitrumGoerli]}
+      defaultChains={[arbitrum]}
       connectorNames={['MetaMask', 'WallectConnect']}
     >
       <ToastProvider>{children}</ToastProvider>
